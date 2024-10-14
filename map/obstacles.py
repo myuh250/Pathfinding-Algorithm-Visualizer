807fa84fcf2dd217c97bf00b4e5cd363ac5b3dfd
“myuh250” <huyng2724@gmail.com>
2024-10-14 13:48:40 +0700

    Update better generate obstacles

--- map/obstacles.py
+++ map/obstacles.py
@@ -10,12 +10,27 @@ class Obstacle:
         self.num_obstacles = num_obstacles
         self.obstacles = []  
         
+    def is_invalid_pattern(self, row, col):
+        patterns = [
+            [(row-1, col-1), (row-1, col), (row, col-1)],  # Pattern 1
+            [(row-1, col+1), (row-1, col), (row, col+1)],  # Pattern 2
+            [(row+1, col-1), (row+1, col), (row, col-1)],  # Pattern 3
+            [(row+1, col+1), (row+1, col), (row, col+1)],  # Pattern 4
+        ]
+        for pattern in patterns:
+            if all(0 <= r < self.rows and 0 <= c < self.cols and (r, c) in self.obstacles for r, c in pattern):
+                return True
+        return False
+        
     def generate_random_obstacles(self):
         self.obstacles = []
-        for _ in range(self.num_obstacles):
+        while len(self.obstacles) < self.num_obstacles:
             row = random.randint(0, self.rows - 1)
             col = random.randint(0, self.cols - 1)
-            self.obstacles.append((row, col))
+            new_obstacle = (row, col)
+
+            if new_obstacle not in self.obstacles and not self.is_invalid_pattern(row, col):
+                self.obstacles.append(new_obstacle)
 
     def draw_obstacles(self, screen):
         for obstacle in self.obstacles:
